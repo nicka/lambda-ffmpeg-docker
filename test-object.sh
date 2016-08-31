b54f85a933d13826b5300310d5e47e38751622b4
Nick den Engelsman <nickdenengelsman@gmail.com>
2016-08-31 18:23:25 +0200

    Add ffprobe to test newly generated files within test-object.sh

--- test-object.sh
+++ test-object.sh
@@ -13,9 +13,15 @@ ffprobe $SAMPLE.$FORMAT -v quiet -show_format -show_streams -of json
 
 # Test FFmpeg
 ffmpeg -loglevel warning -y -i $SAMPLE.$FORMAT -vn -codec:a libmp3lame -b:a 128k $SAMPLE.mp3
+ffprobe $SAMPLE.mp3 -v quiet -show_format -show_streams -of json
+
 ffmpeg -loglevel warning -y -i $SAMPLE.$FORMAT -vn -strict -2 -codec:a libfdk_aac -b:a 96k $SAMPLE.m4a
+ffprobe $SAMPLE.m4a -v quiet -show_format -show_streams -of json
+
 ffmpeg -loglevel warning -y -i $SAMPLE.$FORMAT -vn -codec:a libvorbis $SAMPLE.ogg
-echo ""
+ffprobe $SAMPLE.ogg -v quiet -show_format -show_streams -of json
+
+ls -la
 
 # Remove any generated audio file
 rm $SAMPLE*
